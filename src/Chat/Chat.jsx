7c797bb1ef7a39f70523ed170ceb5ca8ab74de71
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-08-23 22:16:02 +0200

    Navigate to friends if no chat found

--- src/Chat/Chat.jsx
+++ src/Chat/Chat.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { apiDeleteMessage, apiFetchChat } from "../../helpers/API";
 import { useAuthStore } from "../store";
 import MessageForm from "./MessageForm";
@@ -8,6 +8,8 @@ import socket from "../socket";
 
 function Chat() {
     const currentUser = useAuthStore((state) => state.currentUser);
+
+    const navigate = useNavigate();
     const { id } = useParams();
     const [chat, setChat] = useState(null);
 
@@ -33,9 +35,10 @@ function Chat() {
         async function fetchChat() {
             const res = await apiFetchChat(id);
             if (res.status === 200) setChat(res.data.chat);
+            else navigate("/friends");
         }
         fetchChat();
-    }, []);
+    }, [id]);
 
     function addMessage(message) {
         setChat((prevChat) => ({
