059b0813726016ef25ce50d386de66c803f266dc
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-08-29 15:20:16 +0200

    Modify layout and styling of the Navbar

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,37 +1,12 @@
-import { NavLink, Outlet } from "react-router-dom";
-import { useAuthStore } from "../store";
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
 
 function MainLayout() {
-    const logout = useAuthStore((state) => state.logout);
-    const currentUser = useAuthStore((state) => state.currentUser);
-
     return (
-        <div>
-            <nav>
-                <ul>
-                    <li>
-                        <button onClick={logout}>Logout</button>
-                    </li>
-                    <li>
-                        <NavLink to={"/users/" + currentUser._id}>Profile</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/friends">Friends</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/users">Users</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/invites/received">Received Invites</NavLink>
-                    </li>
-
-                    <li>
-                        <NavLink to="/posts">Posts</NavLink>
-                    </li>
-                </ul>
-            </nav>
+        <>
+            <Navbar />
             <Outlet />
-        </div>
+        </>
     );
 }
 
---
+++ src/Layout/Navbar.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import { HiMiniBars3BottomLeft } from "react-icons/hi2";
+import { RiLogoutBoxRLine } from "react-icons/ri";
+import { NavLink } from "react-router-dom";
+import { useAuthStore } from "../store";
+import css from "./styles/Navbar.module.css";
+
+function Navbar() {
+    const logout = useAuthStore((state) => state.logout);
+    const currentUser = useAuthStore((state) => state.currentUser);
+    const [navbarOpen, setNavbarOpen] = useState(false);
+
+    function toggleNavbar() {
+        setNavbarOpen(!navbarOpen);
+    }
+
+    return (
+        <>
+            <button title="Navbar" className={css.toggle} onClick={toggleNavbar}>
+                <HiMiniBars3BottomLeft />
+            </button>
+            <nav className={`${css.nav} ${navbarOpen ? css.open : ""}`}>
+                <button title="Log Out" className={css.logout} onClick={logout}>
+                    <RiLogoutBoxRLine />
+                </button>
+                <ul className={css.list}>
+                    <li>
+                        <NavLink
+                            className={({ isActive }) => (isActive ? css.active : "")}
+                            onClick={toggleNavbar}
+                            to={"/users/" + currentUser._id}
+                            end
+                        >
+                            Profile
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            className={({ isActive }) => (isActive ? css.active : "")}
+                            onClick={toggleNavbar}
+                            to="/friends"
+                        >
+                            Friends
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            className={({ isActive }) => (isActive ? css.active : "")}
+                            onClick={toggleNavbar}
+                            to="/users"
+                            end
+                        >
+                            Users
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            className={({ isActive }) => (isActive ? css.active : "")}
+                            onClick={toggleNavbar}
+                            to="/invites/received"
+                        >
+                            Received Invites
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            className={({ isActive }) => (isActive ? css.active : "")}
+                            onClick={toggleNavbar}
+                            to="/posts"
+                        >
+                            Posts
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </>
+    );
+}
+
+export default Navbar;
