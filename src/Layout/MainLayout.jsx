ddb1549c514491d1f1bfb67c2216b9556d6babfd
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-08-29 15:58:55 +0200

    Wrap Main Layout outlet in container

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
+import css from "./styles/MainLayout.module.css";
 
 function MainLayout() {
     return (
         <>
             <Navbar />
-            <Outlet />
+            <div className={css.container}>
+                <Outlet />
+            </div>
         </>
     );
 }
