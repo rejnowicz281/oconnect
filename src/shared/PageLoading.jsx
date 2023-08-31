81e012f7160a678d34a83564b4a4409f9b9bdf11
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-08-31 13:04:52 +0200

    Add optional 'text' prop to PageLoading

--- src/shared/PageLoading.jsx
+++ src/shared/PageLoading.jsx
@@ -1,7 +1,12 @@
+import PropTypes from "prop-types";
 import css from "./styles/PageLoading.module.css";
 
-function PageLoading() {
-    return <div className={css.loading}>Loading...</div>;
+function PageLoading({ text }) {
+    return <div className={css.loading}>{text ? text : "Loading..."}</div>;
 }
+
+PageLoading.propTypes = {
+    text: PropTypes.string,
+};
 
 export default PageLoading;
